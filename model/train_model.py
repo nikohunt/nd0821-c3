90a4717e05c619e01f34d949e8442f775fcc4627
Nikolas Hunt <nikolas.hunt@dyson.com>
2022-11-22 20:04:12 +0000

    Save model with pickle

--- model/train_model.py
+++ model/train_model.py
@@ -1,14 +1,18 @@
-# Script to train machine learning model.
+"""Script to train machine learning model.
+"""
+import pandas as pd
+import pickle
 
 from sklearn.model_selection import train_test_split
-import pandas as pd
 
-# Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model
 
-# Add code to load in the data.
-data = pd.read_csv("")
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+# Ingest data
+data = pd.read_csv("../data/census.csv")
+
+# Test train split
+train, test = train_test_split(data, test_size=0.20, random_state=42)
 
 cat_features = [
     "workclass",
@@ -20,10 +24,15 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
-
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False
+)
 # Train and save a model.
+model = train_model(X_train, y_train)
+pickle.dump(model, open("model.pkl", "wb"))
