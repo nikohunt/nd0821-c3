ebe4f40d32f66619746e7973b314bd095ba2c291
Nikolas Hunt <nikolas.hunt@dyson.com>
2022-11-22 19:13:31 +0000

    Move ml files out of starter folder

--- model/train_model.py
+++ model/train_model.py
@@ -1,11 +1,12 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
-
+data = pd.read_csv("")
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
