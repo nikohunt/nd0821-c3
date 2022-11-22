b060ccc8b653843e0b9d65fa935ebe88e777c116
Nikolas Hunt <nikolas.hunt@dyson.com>
2022-11-22 23:28:20 +0000

    Add app

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # Put the code for your API here.
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Instantiate the app.
+app = FastAPI()
+
+
+# Setup pytdantic body value
+class Value(BaseModel):
+    value: int
+
+
+@app.get("/")
+async def welcome():
+    return {"greeting": "Welcome to the Census Income Predictor"}
