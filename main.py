401f4e1bad2f6599a315d3047e6d41ecbb8c3dbd
Nikolas Hunt <nikolas.hunt@dyson.com>
2022-11-23 09:33:44 +0000

    Implement inference

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 """FastAPI app that includes pydantic data model for expected incoming
 inference candidate schema, a greeting response for root get requests, and a
-model call for post requests at the /predict endpoint.
+model call for post requests at the /predict endpoint
 """
-
 import pickle
 
+import pandas as pd
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel, Field
 
 # Instantiate the app
 app = FastAPI()
 
 
-# Use of hyphenated feature names in the Respondent class definition below
-# results in `SyntaxError: illegal target for annotation`. We use a function
-# defined here, later added to the class
 def to_hyphen(string: str) -> str:
-    return string.replace("-", "_")
+    """Use of hyphenated feature names in the Respondent class definition below
+    results in `SyntaxError: illegal target for annotation`. We use a function
+    defined here, later added to the class
+
+    Args:
+        string (str): String featuring underscores
+
+    Returns:
+        str: String with underscores replaced by hyphens
+    """
+    return string.replace("_", "-")
 
 
 # Declare example of data inference artifact is expecting using pydantic
@@ -35,7 +43,7 @@ class Respondent(BaseModel):
     capital_loss: int = Field(example=1000)
     hours_per_week: int = Field(example=35)
     native_country: str = Field(example="United-States")
-    salary: int = Field(example=">50k")
+    salary: str = Field(example=">50k")
 
     class Config:
         alias_generator = to_hyphen
@@ -50,11 +58,33 @@ class Response(Respondent):
 clf = pickle.load(open("model/models/model.pkl", "rb"))
 
 
+def pydantic_model_to_df(model_instance: Respondent) -> pd.DataFrame:
+    """Converts pydantic model instance into a Pandas DataFrame which is
+    expected by the model
+
+    Args:
+        model_instance (Respondent): Pydantic class inherited from BaseModel
+
+    Returns:
+        pd.DataFrame: Pandas DataFrame ready for inference
+    """
+    return pd.DataFrame([jsonable_encoder(model_instance)])
+
+
 @app.get("/")
 async def welcome():
     return {"greeting": "Welcome to the Census Income Predictor"}
 
 
-# @app.post("/predict/")
-# async def predict(respondent: Respondent):
-#     return None
+@app.post("/predict/", response_model=Response)
+async def predict(respondent: Respondent):
+    # Convert body to pandas
+    df_instance = pydantic_model_to_df(respondent)
+
+    # Run inference
+    prediction = clf.predict(df_instance).tolist()[0]
+
+    # Construct api response
+    response = respondent.dict(by_alias=True)
+    response.update({"Predictions": prediction})
+    return response
